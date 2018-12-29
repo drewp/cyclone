1181329c6f105d3a30dec44ee080b611ec4ad50a
Marwan Rabbâa <waghanza@gmail.com>
2018-12-29 10:46:19 +0100

    restore original setup.py

--- setup.py
+++ setup.py
@@ -15,74 +15,74 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
+import re
 import sys
 import platform
+import setuptools
 from distutils import log
-from distutils.version import LooseVersion
-from distutils.version import StrictVersion
 
-requires = ["twisted", "pyopenssl"]
+requires = ["twisted"]
 
+def version_cmp(version1, version2):
+    """
+    Return True if version1 is less greater than version2
+    """
+    def normalize(v):
+        return [int(x) for x in re.sub(r'(\.0+)*$','', v).split(".")]
+    return normalize(version1) < normalize(version2)
 
 # Avoid installation problems on old RedHat distributions (ex. CentOS 5)
 # http://stackoverflow.com/questions/7340784/easy-install-pyopenssl-error
 py_version = platform.python_version()
-#if LooseVersion(py_version) < StrictVersion('2.6'):
-#    distname, version, _id = platform.dist()
-#else:
-#    distname, version, _id = platform.linux_distribution()
 
-#is_redhat = distname in ["CentOS", "redhat"]
-#if is_redhat and version and StrictVersion(version) < StrictVersion('6.0'):
-#    requires.append("pyopenssl==0.12")
-#else:
-#    requires.append("pyopenssl")
-
-# PyPy and setuptools don't get along too well, yet.
-if platform.python_implementation().lower().startswith("pypy"):
-    import distutils.core
-    setup = distutils.core.setup
-    extra = dict(extras_require={'ssl': requires})
+if (version_cmp(str(py_version), str('2.6'))):
+    distname, version, _id = platform.dist()
 else:
-    import setuptools
-    setup = setuptools.setup
-    extra = dict(extras_require={'ssl': requires})
+    distname, version, _id = platform.linux_distribution()
 
-    try:
-        from setuptools.command import egg_info
-        egg_info.write_toplevel_names
-    except (ImportError, AttributeError):
-        pass
-    else:
-        """
-        'twisted' should not occur in the top_level.txt file as this
-        triggers a bug in pip that removes all of twisted when a package
-        with a twisted plugin is removed.
-        """
-        def _top_level_package(name):
-            return name.split('.', 1)[0]
+is_redhat = distname in ["CentOS", "redhat"]
+if is_redhat and version and StrictVersion(version) < StrictVersion('6.0'):
+    requires.append("pyopenssl==0.12")
+else:
+    requires.append("pyopenssl")
 
-        def _hacked_write_toplevel_names(cmd, basename, filename):
-            pkgs = dict.fromkeys(
-                [_top_level_package(k)
-                    for k in cmd.distribution.iter_distribution_names()
-                    if _top_level_package(k) != "twisted"
-                ]
-            )
-            cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+extra = dict(extras_require={'ssl': requires})
 
-        egg_info.write_toplevel_names = _hacked_write_toplevel_names
+try:
+    from setuptools.command import egg_info
+    egg_info.write_toplevel_names
+except (ImportError, AttributeError):
+    pass
+else:
+    """
+    'twisted' should not occur in the top_level.txt file as this
+    triggers a bug in pip that removes all of twisted when a package
+    with a twisted plugin is removed.
+    """
+    def _top_level_package(name):
+        return name.split('.', 1)[0]
+
+    def _hacked_write_toplevel_names(cmd, basename, filename):
+        pkgs = dict.fromkeys(
+            [_top_level_package(k)
+                for k in cmd.distribution.iter_distribution_names()
+                if _top_level_package(k) != "twisted"
+            ]
+        )
+        cmd.write_file("top-level names", filename, '\n'.join(pkgs) + '\n')
+
+    egg_info.write_toplevel_names = _hacked_write_toplevel_names
 
 
-setup(
+setuptools.setup(
     name="cyclone",
-    version="1.0",
+    version="1.2",
     author="fiorix",
     author_email="fiorix@gmail.com",
     url="http://cyclone.io/",
     license="http://www.apache.org/licenses/LICENSE-2.0",
-    description="Non-blocking web server. "
-                "A facebook's Tornado on top of Twisted.",
+    description="Non-blocking web server for Python. "
+                "Tornado API as a Twisted protocol.",
     keywords="python non-blocking web server twisted facebook tornado",
     packages=["cyclone", "twisted.plugins", "cyclone.tests", "cyclone.testing"],
     package_data={"twisted": ["plugins/cyclone_plugin.py"],
